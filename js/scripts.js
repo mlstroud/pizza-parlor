508469728ac6881160815acf6045c46a22121bd8
Matt Stroud <lixingke06@gmail.com>
2020-04-10 09:04:01 -0700

    add prototype to update pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,32 @@ function Pizza(pizzaSize, pizzaToppings) {
   this.price = 0;
 }
 
+Pizza.prototype.updatePrice = function() {
+  switch(this.size) {
+    case "Small":
+      this.price = 10;
+      break;
+    case "Medium":
+      this.price = 12;
+      break;
+    case "Large":
+      this.price = 14;
+      break;
+    default:
+      this.price = 0;
+      break;
+  }
 
+  var extraToppingCount = this.toppings.length - 3;
+
+  if(extraToppingCount > 0) {
+    this.price += extraToppingCount;
+  }
+
+  if(this.toppings.includes("Pan Pizza")) {
+    this.price += 2;
+  }
+}
 
 
 
