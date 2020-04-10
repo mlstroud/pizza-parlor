1b1b3e5e36657cc099516407b717a6489d100d65
Matt Stroud <lixingke06@gmail.com>
2020-04-10 08:58:32 -0700

    add constructor to create pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
+// Business Logic = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+function Pizza(pizzaSize, pizzaToppings) {
+  this.size = pizzaSize;
+  this.toppings = pizzaToppings;
+  this.price = 0;
+}
 
+
+
+
+
+// User Interface = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 $(document).ready(function() {
 
 });
