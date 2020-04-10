3424e5e170e81580d7a0be4a0151a26276da9527
Matt Stroud <lixingke06@gmail.com>
2020-04-10 16:24:36 -0700

    fix cart to display multiple pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -9,17 +9,24 @@ Order.prototype.add = function(item) {
 
 Order.prototype.show = function() {
   var outputHTML = "";
+  var totalPrice = 0;
 
-  for(var index = 1; index <= this.items.length; i++) {
+  for(var index = 0; index < this.items.length; index++) {
     outputHTML += 
-    "<h2>Pizza " + index + "</h2>" +
-    "Size: " + this.items.size + "<br>" +
-    "Crust: " + this.items.crust + "<br>" +
-    "Cheese: " + this.items.cheese + "<br>" +
-    "Meat: " + this.items.meat.join(", ") + "<br>" +
-    "Veggies" + this.items.veggies.join(", ") + "<br>";
+    "<hr><h2>Pizza " + (index+1) + "</h2>" +
+    "Size: " + this.items[index].size + "<br>" +
+    "Crust: " + this.items[index].crust + "<br>" +
+    "Cheese: " + this.items[index].cheese + "<br>" +
+    "Meat: " + this.items[index].meat.join(", ") + "<br>" +
+    "Veggies: " + this.items[index].veggies.join(", ") + "<br>";
+    totalPrice += this.items[index].price;
   }
-  
+
+  if($("input:radio[name='order-type']:checked").val() === "delivery"){
+    totalPrice += 5;
+    outputHTML += "<br>Delivery Fee: $5.00";
+  }
+  $("#current-price").html("Total $" + totalPrice.toFixed(2));
   return outputHTML;
 }
 
@@ -38,13 +45,13 @@ function Pizza(pizzaSize, pizzaCrust, pizzaCheese, pizzaMeat, pizzaVeggies) {
 
 Pizza.prototype.updatePrice = function() {
   switch(this.size) {
-    case "small":
+    case "Small":
       this.price = 9.99;
       break;
-    case "medium":
+    case "Medium":
       this.price = 11.99;
       break;
-    case "large":
+    case "Large":
       this.price = 13.99;
       break;
     default:
@@ -58,22 +65,20 @@ Pizza.prototype.updatePrice = function() {
     this.price += extraToppingCount;
   }
 
-  if(this.crust === "panpizza") {
+  if(this.crust === "Pan Pizza") {
     this.price += 2;
   }
 
-  if(this.cheese === "extra") {
+  if(this.cheese === "Extra") {
     this.price += 1.50;
   }
 
   this.price *= 1.10;
-
-  console.log(this.price.toFixed(2));
 }
 
 // User Interface = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
-function createPizza(order) {
+function createPizza() {
   var size = $("input[name=size]:checked").val();
   var crust = $("input[name=crust]:checked").val();
   var cheese = $("input[name=cheese]:checked").val();
@@ -91,7 +96,7 @@ function createPizza(order) {
   var pizza = new Pizza(size, crust, cheese, meat, veggies);
   pizza.updatePrice();
 
-  order.add(pizza);
+  return pizza;
 }
 
 function showOrderForm(pizzaName) {
@@ -121,61 +126,61 @@ function showOrderForm(pizzaName) {
   "<h2>Size</h2>" +
   "<div class='radio'>" + 
   " <label>" +
-  "   <input  type='radio' name='size' value='small' checked> Small" +
+  "   <input  type='radio' name='size' value='Small' checked> Small" +
   " </label>" +
   "</div>" +
   "<div class='radio'>" + 
   " <label>" +
-  "   <input  type='radio' name='size' value='medium'> Medium" +
+  "   <input  type='radio' name='size' value='Medium'> Medium" +
   " </label>" +
   "</div>" +
   "<div class='radio'>" + 
   " <label>" +
-  "   <input  type='radio' name='size' value='large'> Large" +
+  "   <input  type='radio' name='size' value='Large'> Large" +
   " </label>" +
   "</div>" +
   "<h2>Crust</h2>" +
   "<div class='radio'>" + 
   " <label>" +
-  "   <input  type='radio' name='crust' value='handtossed' checked> Hand Tossed" +
+  "   <input  type='radio' name='crust' value='Hand Tossed' checked> Hand Tossed" +
   " </label>" +
   "</div>" +
   "<div class='radio'>" + 
   " <label>" +
-  "   <input  type='radio' name='crust' value='panpizza'> Pan Pizza" +
+  "   <input  type='radio' name='crust' value='Pan Pizza'> Pan Pizza" +
   " </label>" +
   "</div>" +
   "<h2>Cheese</h2>" +
   "<div class='radio'>" + 
   " <label>" +
-  "   <input  type='radio' name='cheese' value='normal' checked> Normal" +
+  "   <input  type='radio' name='cheese' value='Normal' checked> Normal" +
   " </label>" +
   "</div>" +
   "<div class='radio'>" + 
   " <label>" +
-  "   <input  type='radio' name='cheese' value='extra'> Extra" +
+  "   <input  type='radio' name='cheese' value='Extra'> Extra" +
   " </label>" +
   "</div>" +
 
   "<h2>Meat</h2>" +
   "<div>" +
-  " <input type='checkbox' name='meat' id='beef' value='beef'>" +
+  " <input type='checkbox' name='meat' id='beef' value='Beef'>" +
   " <label for='beef'>Beef</label><br>" +
-  " <input type='checkbox' name='meat' id='canadianbacon' value='canadianbacon'>" +
+  " <input type='checkbox' name='meat' id='canadianbacon' value='Canadian Bacon'>" +
   " <label for='canadianbacon'>Canadian Bacon</label><br>" +
-  " <input type='checkbox' name='meat' id='pepperoni' value='pepperoni'>" +
+  " <input type='checkbox' name='meat' id='pepperoni' value='Pepperoni'>" +
   " <label for='pepperoni'>Pepperoni</label><br>" +
   "</div>" +
 
   "<h2>Veggies</h2>" +
   "<div>" +
-  " <input type='checkbox' name='veggies' id='olives' value='olives'>" +
+  " <input type='checkbox' name='veggies' id='olives' value='Olives'>" +
   " <label for='olives'>Olives</label><br>" +
-  " <input type='checkbox' name='veggies' id='mushrooms' value='mushrooms'>" +
+  " <input type='checkbox' name='veggies' id='mushrooms' value='Mushrooms'>" +
   " <label for='mushrooms'>Mushrooms</label><br>" +
-  " <input type='checkbox' name='veggies' id='tomatoes' value='tomatoes'>" +
+  " <input type='checkbox' name='veggies' id='tomatoes' value='Mushrooms'>" +
   " <label for='tomatoes'>Tomatoes</label><br>" +
-  " <input type='checkbox' name='veggies' id='peppers' value='peppers'>" +
+  " <input type='checkbox' name='veggies' id='peppers' value='Mushrooms'>" +
   " <label for='peppers'>Peppers</label><br>" +
   "</div>";
 
@@ -195,12 +200,14 @@ function attachListeners() {
   });
 
   $("button#addtocart").click(function() {
-    var order = new Order();
-    createPizza(order);
+    var pizza = createPizza();
+    order.add(pizza);
+    $("#cart").text("");
     $("#cart").html(order.show());
     $("#cart").slideDown();
   });
 }
+var order = new Order();
 
 $(document).ready(function() {
   attachListeners();
